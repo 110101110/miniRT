71404703ca4a5bbf8e3a39abf037bab9446623ed
nok <visouthivong.kevin@gmail.com>
2026-01-20 13:30:50 +0100

    structure ready

--- includes/parser.h
+++ includes/parser.h
@@ -6,46 +6,82 @@
 /*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 13:05:42 by kevisout          #+#    #+#             */
-/*   Updated: 2026/01/17 13:30:49 by kevisout         ###   ########.fr       */
+/*   Updated: 2026/01/20 13:30:33 by kevisout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #define BUFFER_SIZE 32
 
-typedef struct ambient_light
-{
-	float	ratio;
-	int		r;
-	int		g;
-	int		b;
-}           t_ambient_light;
+// UNIQUE ELEMENTS STRUCTS
 
-typedef struct cam
+typedef struct s_ambient
 {
-	t_vec3	origin;
-	t_vec3	direction;
-	float	fov;
-}           t_cam;
+	double	ratio;
+	t_color	color;
+}	t_ambient;
 
-typedef struct light
+typedef struct s_camera
 {
-	t_vec3	position;
-	float	ratio;
-	int		r;
-	int		g;
-	int		b;
-}		   t_light;
+	t_vec3	pos;
+	t_vec3	orient;
+	int		fov;
+}	t_camera;
 
-typedef struct s_parse
+typedef struct s_light
 {
-	t_ambient_light	ambient;
-	t_cam			camera;
-	t_light			light;
-	int		sphere_count;
-	int		plane_count;
-	int		cylinder_count;
-}				t_parse;
+	t_vec3	pos;
+	double	ratio;
+	t_color	color;
+}	t_light;
 
-int parse(int ac, char **av);
+// SHAPE STRUCTS
+
+typedef enum e_type
+{
+	SPHERE,
+	PLANE,
+	CYLINDER
+}	t_type;
+
+typedef struct s_sphere
+{
+	t_vec3	center;
+	double	diameter;
+	t_color	color;
+}	t_sphere;
+
+typedef struct s_plane
+{
+	t_vec3	point;
+	t_vec3	normal;
+	t_color	color;
+}	t_plane;
+
+typedef struct s_cylinder
+{
+	t_vec3	center;
+	t_vec3	axis;
+	double	diameter;
+	double	height;
+	t_color	color;
+}	t_cylinder;
+typedef struct s_shape
+{
+	t_type			type;
+	void			*element;
+	struct s_shape	*next;
+}	t_shape;
+
+// MAIN DATA STRUCT
+
+typedef struct s_data_parsing
+{
+	t_ambient		*ambient;
+	t_camera		*camera;
+	t_light			*light;
+	t_shape			*shapes;
+}	t_data_parsing;
+
+int		parse(int ac, char **av);
 char	*get_next_line(int fd);
